dc95f5c03ef96f467929189f4a528cdd5e7ffd2f
jebomin <jebomin0515@naver.com>
2023-08-03 20:05:22 +0900

    Constants

---
+++ src/chapter_01/_06_Constants.java
@@ -0,0 +1,17 @@
+package chapter_01;
+
+import java.sql.SQLOutput;
+
+public class _06_Constants {
+    public static void main(String[] args) {
+        //fianl 넣어서 상수로 만들기
+        final String KR_COUNTRY_CODE = "+82";  //국가 번호(빨리빨리의 민족!)
+        System.out.println(KR_COUNTRY_CODE);
+
+        final double PI = 3.141592; //원주율
+        System.out.println(PI);
+
+        final String DATE_OF_BIRTH = "2001-05-15";//생년월일
+        System.out.println(DATE_OF_BIRTH);
+    }
+}
