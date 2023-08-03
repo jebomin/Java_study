a2267c68f871a07558c07901a6693f543318f2a9
jebomin <jebomin0515@naver.com>
2023-08-03 19:32:18 +0900

    Comment

---
+++ src/chapter_01/_04_Comment.java
@@ -0,0 +1,24 @@
+package chapter_01;
+
+public class _04_Comment {
+    public static void main(String[] args) {
+        System.out.println("(10분 전) 잠시 후 결혼식 시작 예정이오니 착석 부탁드립니다.");
+        //ctrl+d 누르면 똑같은 문장이 생김
+        //System.out.println("(5분 전) 잠시 후 결혼식 시작 예정이오니 착석 부탁드립니다.");
+        System.out.println("지금부터 결혼식을 시작하겠습니다.");
+
+        int size = 120;
+        size = size+10; //어린이는 발이 빨리 자라기 때문에 사이즈 10만큼 더 큰 신발로 구매
+        System.out.println("신발 사이즈 "+size+"으로 보여주세요!");
+
+        /*
+        int a = 10;
+        int b = 20;
+        System.out.println(a+b); //30
+         */
+        //한줄 주석 단축키 : ctrl+/(한번 더 쓰면 주석 해제)
+        //여러 줄 주석 단축키 : 주석 원하는 부분 드래그 하고 ctrl+shift+/(한번 더 쓰면 주석 해제)
+        //만약 단축키 잘 안먹으면 한영키 한번 누르면 잘 먹음!
+
+    }
+}
