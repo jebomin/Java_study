c2007a2289bd139874fa79343d7bf3331445970d
jebomin <jebomin0515@naver.com>
2023-08-03 20:50:31 +0900

    Quiz_01

---
+++ src/chapter_01/_Quiz_01.java
@@ -0,0 +1,13 @@
+package chapter_01;
+
+public class _Quiz_01 {
+    public static void main(String[] args) {
+        String busNumber_2 = "상암 08";
+        int time_1 = 3;
+        double length_1 = 1.2;
+
+        System.out.println(busNumber_2+"번 버스");
+        System.out.println("약 "+time_1+"분 후 도착");
+        System.out.println("남은 거리 "+length_1+"km");
+    }
+}
