eeb6b88d67377356853647bdea554d6b2cf2de0b
jebomin <jebomin0515@naver.com>
2023-08-13 00:33:59 +0900

    Continue_주석추가

--- src/chapter_04/_11_Continue.java
+++ src/chapter_04/_11_Continue.java
@@ -31,7 +31,7 @@ public class _11_Continue {
         sold = 0;
         int index = 0; // 손님 번호
         // while (index <= 50) {
-        while (true) {
+        while (true) { //break로 인해 탈출 조건이 있어서 true라 적어도 괜찮음
             index++;
             System.out.println(index + "번 손님, 주문하신 치킨 나왔습니다.");
 
