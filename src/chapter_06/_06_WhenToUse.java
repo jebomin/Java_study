12d17bcec0f7cc296520291e822df7997f77b770
jebomin <jebomin0515@naver.com>
2023-08-15 21:41:02 +0900

    메소드가 필요한 이유

---
+++ src/chapter_06/_06_WhenToUse.java
@@ -0,0 +1,23 @@
+package chapter_06;
+
+public class _06_WhenToUse {
+    public static int getPower(int number, int exponent) {
+        int result = 1;
+        for (int i = 0; i < exponent; i++) {
+            result *= number;
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        //메소드가 필요한 이유
+
+        //2의 2승을 구하기
+        System.out.println(getPower(2,2)); //2*2=4
+
+        //3의 3승을 구하기
+        System.out.println(getPower(3,3)); //3*3*3=27
+
+        //4의 2승을 구하기
+        System.out.println(getPower(4,2)); //4*4=16
+    }
+}
