ed587cd07e756fb348eba40121ab4f1928c6612b
jebomin <jebomin0515@naver.com>
2023-08-20 23:13:22 +0900

    패키지

--- src/chapter_08/_00_AccessModifierTest.java
+++ src/chapter_08/_00_AccessModifierTest.java
@@ -1,6 +1,6 @@
 package chapter_08;
 
-import chapter_07.BlackBoxRefurbish;
+import chapter_07.*;
 
 public class _00_AccessModifierTest {
     public static void main(String[] args) {
@@ -9,5 +9,7 @@ public class _00_AccessModifierTest {
         //b1.resolution = "FHD"; //default
         //b1.price - 200000; //private
         //b1.color = "블랙"; //protected
+
+        Blackbox b2;
     }
 }
