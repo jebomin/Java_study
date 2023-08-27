189fa7f6f093f5696704e635131cba06e1cbe1f0
jebomin <jebomin0515@naver.com>
2023-08-27 22:28:43 +0900

    WrapperClass

---
+++ src/chapter_09/_03_WrapperClass.java
@@ -0,0 +1,27 @@
+package chapter_09;
+
+public class _03_WrapperClass {
+    public static void main(String[] args) {
+        //래퍼(Wrapper) 클래스
+        //기본 자료형(int, double, float, char)을 객체의 형태로 만들어서 사용
+
+        Integer i = 123; //int i = 123;
+        Double d = 1.0; //double d = 1.0;
+        Character c = 'A'; //char c = 'A';
+
+        System.out.println(i);
+        System.out.println(d);
+        System.out.println(c);
+
+        System.out.println("---------------------");
+
+        System.out.println(i.intValue());
+        System.out.println(d.intValue());
+        System.out.println(c.charValue());
+
+        System.out.println("---------------------");
+
+        String s = i.toString();
+        System.out.println(s);
+    }
+}
