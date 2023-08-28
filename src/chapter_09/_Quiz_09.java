70141ce0760320b56052ca86eeb07678ebcab890
jebomin <jebomin0515@naver.com>
2023-08-28 11:22:37 +0900

    quiz9

---
+++ src/chapter_09/_Quiz_09.java
@@ -0,0 +1,32 @@
+package chapter_09;
+
+import java.util.ArrayList;
+
+public class _Quiz_09 {
+    public static void main(String[] args) {
+        ArrayList<Student> list = new ArrayList<>();
+        list.add(new Student("유재석", "파이썬"));
+        list.add(new Student("박명수", "자바"));
+        list.add(new Student("김종국", "자바"));
+        list.add(new Student("조세호", "C"));
+        list.add(new Student("서장훈", "파이썬"));
+
+        System.out.println("자바 자격증을 보유한 학생");
+        System.out.println("---------------------");
+        for (Student student:list) {
+            if(student.certification.equals("자바")) {
+                System.out.println(student.name);
+            }
+        }
+    }
+}
+
+class Student {
+    public String name;
+    public String certification;
+
+    public Student(String name, String certification) {
+        this.name = name;
+        this.certification = certification;
+    }
+}
