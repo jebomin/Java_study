7d741b40790aa6702279b9cce0e6890c9115df5e
jebomin <jebomin0515@naver.com>
2023-08-29 11:51:21 +0900

    익명 클래스(후반전)

---
+++ src/chapter_10/_02_AnonymousClass2.java
@@ -0,0 +1,36 @@
+package chapter_10;
+
+public class _02_AnonymousClass2 {
+    public static void main(String[] args) {
+        HomeMadeBurger momMadeBurger = getMomMadeBurger();
+        momMadeBurger.cook();
+        System.out.println("---------------------------");
+
+        HomeMadeBurger broMadeBurger = getBroMadeBurger();
+        broMadeBurger.cook();
+    }
+
+    private static HomeMadeBurger getBroMadeBurger() {
+        return new HomeMadeBurger() {
+            @Override
+            public void cook() {
+                System.out.println("집에서 만드는 동생표 군대리아");
+                System.out.println("재료 : 빵, 치킨패티, 양배추 샐러드, 딸기잼, 치즈, 삶은 계란");
+            }
+        };
+    }
+
+    public static HomeMadeBurger getMomMadeBurger() {
+        return new HomeMadeBurger() {
+            @Override
+            public void cook() {
+                System.out.println("집에서 만드는 엄마표 수제 버거");
+                System.out.println("재료 : 빵, 소고기 패티, 해시 브라운, 양상추, 마요네즈, 피클");
+            }
+        };
+    }
+}
+
+abstract class HomeMadeBurger {
+    public abstract void cook();
+}
