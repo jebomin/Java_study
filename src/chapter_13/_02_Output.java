c1e5132019488c38fee2de61930f6b9d1f7e992a
jebomin <jebomin0515@naver.com>
2023-09-04 14:34:05 +0900

    출력(후반전-실수)

--- src/chapter_13/_02_Output.java
+++ src/chapter_13/_02_Output.java
@@ -15,5 +15,13 @@ public class _02_Output {
         System.out.printf("%+6d%n", 1234); //_+1234 (항상 기호 표시)
         System.out.printf("%,15d%n",1000000000); // __1,000,000,000 (세자리마다 콤마로 구분)
         System.out.printf("%-6d%n", 1234); //1234__ (6자리 공간을 확보하고 나서 왼쪽 정렬 1234 출력)
+
+        System.out.println("--------실수--------");
+        System.out.printf("%f%n", Math.PI); //3.142593 (일반 출력)
+        System.out.printf("%.2f%n", Math.PI); //3.14 (소수점 둘째자리)
+        System.out.printf("%6.2fn", Math.PI); //__3.14 (6자리 공간 확보하고 소수점 둘째자리)
+        System.out.printf("%-6.2f%n", Math.PI); //3.14__ (왼쪽 정렬)
+        System.out.printf("%06.2f%n", Math.PI); //003.14 (빈 공간을 0으로 채우기)
+        System.out.printf("%+6.2f%n", Math.PI); //_+3.14 (항상 += 기호 표시)
     }
 }
